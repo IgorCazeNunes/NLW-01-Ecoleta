e65049042ad4d9455f72fd645c43ef944e164ce3
Igor Cazé Nunes <igorcaze@gmail.com>
2020-09-15 14:19:30 -0300

    Finalizado projeto mobile.

--- server/src/controllers/pointsController.ts
+++ server/src/controllers/pointsController.ts
@@ -17,7 +17,13 @@ class PointsController {
             .distinct()
             .select('points.*');
 
-        return response.json({ points });
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,
+            };
+        });
+
+        return response.json(serializedPoints);
     }
 
     async show(request: Request, response: Response) {
@@ -26,7 +32,7 @@ class PointsController {
         const point = await knex('points').where('id', id).first();
 
         if (!point) {
-            return response.status(400).json({ message: 'Point not found.'})
+            return response.status(400).json({ message: 'Point not found.' })
         }
 
         const items = await knex('items')
@@ -48,7 +54,7 @@ class PointsController {
             uf,
             items
         } = request.body
-    
+
         const point = {
             image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
             name,
@@ -61,25 +67,25 @@ class PointsController {
         };
 
         const trx = await knex.transaction();
-        
+
         const insertedIds = await trx('points').insert(point);
-    
+
         const point_id = insertedIds[0];
-    
+
         const pointItems = items.map((item_id: number) => {
             return {
                 item_id,
                 point_id,
             }
         })
-    
+
         await trx('point_items').insert(pointItems);
 
         await trx.commit();
-    
-        return response.json({ 
+
+        return response.json({
             id: point_id,
-            ...point, 
+            ...point,
         });
     }
 }
